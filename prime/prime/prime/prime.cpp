81c0cb4666c42759e10552e420053148638ce940
shaiel <shaiel>
2025-06-17 21:57:49 +0300

    fixed according to comment

--- prime/prime/prime/prime.cpp
+++ prime/prime/prime/prime.cpp
@@ -6,23 +6,9 @@ using std::cout;
 using std::endl;
 
 int main() {
-    int number = 7;
-    cout << number << " is " << (checkIfPrime(number) ? "Prime" : "Not prime")
-         << endl;
-    number = 0;
-    cout << number << " is " << (checkIfPrime(number) ? "Prime" : "Not prime")
-         << endl;
-    number = 1;
-    cout << number << " is " << (checkIfPrime(number) ? "Prime" : "Not prime")
-         << endl;
-    number = 9;
-    cout << number << " is " << (checkIfPrime(number) ? "Prime" : "Not prime")
-         << endl;
-    number = 10;
-    cout << number << " is " << (checkIfPrime(number) ? "Prime" : "Not prime")
-         << endl;
-    number = -2;
-    cout << number << " is " << (checkIfPrime(number) ? "Prime" : "Not prime")
-         << endl;
+    for (int number = -3; number < 11; number++) {
+        cout << number << " is "
+             << (checkIfPrime(number) ? "Prime" : "Not prime") << endl;
+    }
     return 0;
 }
