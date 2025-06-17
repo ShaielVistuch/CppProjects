d5376ecf9a23a9db301b08ad0c3a6c5d685a3f13
shaiel <shaiel>
2025-06-17 20:32:44 +0300

    prime wip1

---
+++ prime/prime/prime/prime.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using std::cout;
+
+int main() { 
+	int number = 7;
+    cout << checkIfPrime();
+	return 0;
+}
---
+++ prime/prime/prime/primtUtils.cpp
@@ -0,0 +1,5 @@
+#include "primeUtils.h"
+
+int checkIfPrime(int number) {
+
+}
