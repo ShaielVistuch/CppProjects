f39d7713a8c8c660567f3e557eb21a3a51c2a904
shaiel <shaiel>
2025-06-30 18:55:36 +0300

    finished ruth

--- Ruth/Ruth/Ruth/sendSignals.cpp
+++ Ruth/Ruth/Ruth/sendSignals.cpp
@@ -1,38 +1,36 @@
 #include <iostream>
 #include <signal.h>
-#include <unistd.h>
+#include <string>
 
+using std::cin;
 using std::cout;
 using std::endl;
-
-void signalHandeler(int signal) {
-  switch (signal) {
-  case SIGINT:
-  case SIGTERM:
-    printf("Terminating the program (recieved signal #%d)", signal);
-    exit(0);
-  case SIGUSR1:
-    printf("Joanne Rowling, known by her pen name J. K. Rowling, is a British "
-           "author and philanthropist. She is the author of Harry Potter, a "
-           "seven-volume fantasy novel series published from 1997 to 2007.");
-    break;
-  case SIGUSR2:
-    printf("John Ronald Reuel Tolkien was an English writer and philologist. "
-           "He was the author of the high fantasy works The Hobbit and The "
-           "Lord of the Rings. From 1925 to 1945 Tolkien was the Rawlinson and "
-           "Bosworth Professor of Anglo-Saxon and a Fellow of Pembroke "
-           "College, both at the University of Oxford.");
-    break;
-  }
-}
+using std::string;
 
 int main() {
-  signal(SIGINT, signalHandeler);
-  signal(SIGTERM, signalHandeler);
-  signal(SIGUSR1, signalHandeler);
-  signal(SIGUSR2, signalHandeler);
+  int PID;
+  string signal;
+  cout << "Enter PID: " << endl;
+  cin >> PID;
   while (true) {
-    printf("I am alive\n");
-    sleep(2);
+    cout << "Which signal you wish to send? (SIGINT, SIGTERM, SIGUSR1, or "
+            "SIGUSR2)"
+         << endl;
+    cin >> signal;
+    if (signal == "SIGINT") {
+      kill(PID, SIGINT);
+      exit(0);
+    } else if (signal == "SIGTERM") {
+      kill(PID, SIGTERM);
+      exit(0);
+    } else if (signal == "SIGUSR1") {
+      kill(PID, SIGUSR1);
+    } else if (signal == "SIGUSR2") {
+      kill(PID, SIGUSR2);
+    } else {
+      cout << "Signal entered is invalid or not yet supported. Please type:  "
+              "SIGINT, SIGTERM, SIGUSR1, or SIGUSR2."
+           << endl;
+    }
   }
 }
