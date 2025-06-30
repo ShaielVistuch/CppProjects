9b0c6e9c4b5882e707bac72905b37bb76f142c39
shaiel <shaiel>
2025-06-30 18:56:14 +0300

    finished ruth

--- Ruth/Ruth/Ruth/Ruth.cpp
+++ Ruth/Ruth/Ruth/Ruth.cpp
@@ -6,30 +6,27 @@ using std::cout;
 using std::endl;
 
 void signalHandeler(int signal) {
-  switch (signal) {}
-  if (signal == SIGINT) {
-    cout << "Recieved SIGINT (code " << signal << ")" << endl;
-    exit(SIGINT);
-  } else if (signal == SIGTERM) {
-    cout << "Recieved SIGTERM (code " << signal << ")" << endl;
-    exit(SIGTERM);
-  } else if (signal == SIGUSR1) {
-    cout << "Joanne Rowling, known by her pen name J. K. Rowling, is a British "
-            "author and philanthropist. She is the author of Harry Potter, a "
-            "seven-volume fantasy novel series published from 1997 to 2007."
-         << endl;
-  } else if (signal == SIGUSR2) {
-    cout << "John Ronald Reuel Tolkien was an English writer and philologist. "
-            "He was the author of the high fantasy works The Hobbit and The "
-            "Lord of the Rings. From 1925 to 1945 Tolkien was the Rawlinson "
-            "and Bosworth Professor of Anglo-Saxon and a Fellow of Pembroke "
-            "College, both at the University of Oxford. "
-         << endl;
+  switch (signal) {
+  case SIGINT:
+  case SIGTERM:
+    printf("Terminating the program (recieved signal #%d)", signal);
+    exit(0);
+  case SIGUSR1:
+    printf("Joanne Rowling, known by her pen name J. K. Rowling, is a British "
+           "author and philanthropist. She is the author of Harry Potter, a "
+           "seven-volume fantasy novel series published from 1997 to 2007.");
+    break;
+  case SIGUSR2:
+    printf("John Ronald Reuel Tolkien was an English writer and philologist. "
+           "He was the author of the high fantasy works The Hobbit and The "
+           "Lord of the Rings. From 1925 to 1945 Tolkien was the Rawlinson and "
+           "Bosworth Professor of Anglo-Saxon and a Fellow of Pembroke "
+           "College, both at the University of Oxford.");
+    break;
   }
 }
 
 int main() {
-
   signal(SIGINT, signalHandeler);
   signal(SIGTERM, signalHandeler);
   signal(SIGUSR1, signalHandeler);
